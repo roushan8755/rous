ebbeeaab6d696bc456c32ba750d3a662147a51b4
roushan <roushan.kumar@maventechlabs.com>
2023-09-19 08:49:05 +0530

    test the async/await code

---
+++ index.js
@@ -0,0 +1,23 @@
+const url = 'https://jsonplaceholder.typicode.com/posts';
+
+// const getData = () => {
+//     let x = fetch(url).then(res => res.json()).then(res => console.log(res))
+//     return x;
+// }
+
+// let data = getData();
+// console.log(data);
+
+const getData = async () => {
+    try {
+        const res = await fetch(url);
+        const dt = await res.json();
+        console.log(dt);
+    } catch (err) {
+        console.error(err)
+    }
+
+
+    // console.log(res);
+}
+getData();
